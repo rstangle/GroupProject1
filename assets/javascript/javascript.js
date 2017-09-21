f84de070adc48addcc761346fe7504ff079a17aa
Stangle <RStangle@firstam.com>
2017-09-21 18:38:53 -0400

    User Log In Edits

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -68,6 +68,8 @@ firebase.auth().onAuthStateChanged(function(user){
 		currentUser = user.displayName;
 		console.log(currentUser);
 		$("#auth").hide();
+		$("#login").hide();
+		$("#main-menu-image").show();
 		$(".page-header").show();
 		userRef = database.ref(currentUser);
 		userRef.onDisconnect().remove();
@@ -151,7 +153,7 @@ window.onload = function() {
 };
 $("#start").on("click", function(){
 	//call video modal
-	$("#ytplayer").attr("src", "https://www.youtube.com/embed/rmznTYTPINc?autoplay=1&controls=0&end=27&modestbrandding=1&disablekb=1&enablejsapi=1&rel=0&showinfo=0&origin=http://example.com");
+	$("#ytplayer").attr("src", "https://www.youtube.com/embed/rmznTYTPINc?autoplay=1&controls=0&end=28&modestbrandding=1&disablekb=1&enablejsapi=1&rel=0&showinfo=0&origin=http://example.com");
 
 });
 
@@ -159,7 +161,7 @@ $("#startCinematic").on("shown.bs.modal", function(){
 		console.log("shown");
 		pauseAudio();
 		if(randomHeros.length !=0){
-		timeOut = setTimeout(modalcontrol, 26000);
+		timeOut = setTimeout(modalcontrol, 28000);
 		}
 
 	});
@@ -644,7 +646,7 @@ function decrement() {
 					
 					$("#modalIntergame").modal("show");//shows intitial modal for now
 					// console.log(userRef.currentUser);
-					$("#win-lose-message").html("<h3> Congratulations! You saved " + currentHero + ".</h3>")
+					$("#win-lose-message").html("<h3> Congratulations! You saved " + currentHero.toUpperCase() + ".</h3>")
 					createImageDiv("saves");
 					userRef.transaction(function(user){
 
@@ -658,7 +660,7 @@ function decrement() {
 
 					
 					$("#modalIntergame").modal("show");
-					$("#win-lose-message").html("<h3>DANGER TRUE BELIEVERS! You lost " + currentHero + ".</h3>")
+					$("#win-lose-message").html("<h3>DANGER TRUE BELIEVERS! You lost " + currentHero.toUpperCase() + ".</h3>")
 					createImageDiv("losses");
 					userRef.transaction(function(user){
 
